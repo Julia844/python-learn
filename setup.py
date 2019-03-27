476db55dafbe671e342d6ebe573eec76ad49d2a4
yefang <yefang0224@tpl.cntaiping>
2019-03-27 15:43:52 +0800

    解析文件，写入数据库

--- setup.py
+++ setup.py
@@ -3,7 +3,17 @@
 
 import pandas as pd
 import sys
-
+import os
+import logging
+import logging.config
 
 if __name__=="__main__":
   print("abc")
+  print(os.path.join(os.getcwd(),'data', 'log.txt'))
+  logging.basicConfig(filename = os.path.join(os.getcwd(),"data", 'log.txt'), level = logging.DEBUG)
+  log = logging.getLogger('root')
+  log.debug('%s, %s, %s', *('error', 'debug', 'info'))
+  log.debug('%(module)s, %(info)s', {'module': 'log', 'info': 'error'})
+  log.info("acc")
+  log.error("error")
+  print("bcd")
